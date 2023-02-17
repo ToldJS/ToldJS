987e9325b379273eeb0550e07718a17d403ac71d
Rumrobot <rumrobot@gmail.com>
2023-02-17 21:07:45 +0100

    Some stuff changed
    Commit to test the docker container. Removed stuff vite came with, and other standard things.

--- ToldJS/src/main.ts
+++ ToldJS/src/main.ts
@@ -1,5 +1,5 @@
 import './app.css'
-import App from './App.svelte'
+import App from './routes/index.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
---
+++ ToldJS/src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import svelteLogo from '../assets/svelte.svg'
+  import Counter from '../lib/Counter.svelte'
+
+  
+</script>
+
+<main>
+
+</main>
+
+<style>
+
+</style>
