d39b8ac414b55cb9e35c156d971d2b12432a99ef
Johnny JTH <johanohly@gmail.com>
2023-03-08 13:17:10 +0100

    Ny navbar og logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,40 @@
 </script>
 
 <html data-theme="postlort" lang="da" />
+
 <div class="navbar bg-base-100">
-	<div class="navbar-start" />
-	<div class="navbar-center">
-		<a href="/" class="btn btn-ghost normal-case text-xl">ToldJS</a>
-		<a href="/om-os" class="btn btn-ghost normal-case text-xl">Om Projektet</a>
+	<div class="navbar-start">
+		<div class="dropdown">
+			<label tabindex="0" class="btn btn-ghost lg:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h8m-8 6h16"
+					/></svg
+				>
+			</label>
+			<ul
+				tabindex="0"
+				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+			>
+				<li><a href="/generator">Generator</a></li>
+				<li><a href="/om-os">Om Projektet</a></li>
+			</ul>
+		</div>
+		<a href="/" class="btn btn-ghost"><img class="h-14" src="/ToldJS_White.png" alt="ToldJS Logo" /></a>
+	</div>
+	<div class="navbar-center hidden lg:flex">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="/generator">Generator</a></li>
+			<li><a href="/om-os">Om Projektet</a></li>
+		</ul>
 	</div>
 	<div class="navbar-end" />
 </div>
--- src/routes/om-os/+page.svelte
+++ src/routes/om-os/+page.svelte
@@ -1,5 +1,2 @@
-<script lang="ts">
-    export let data;
-</script>
-Your mom
-{JSON.stringify(data)}
+Del af logo:
+<a href="https://commons.wikimedia.org/wiki/File:Airport_Customs.svg">京市</a>, <a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>, via Wikimedia Commons
