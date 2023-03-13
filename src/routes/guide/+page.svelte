a3fd51f3630ae75d4949a6684e33a1b985a0a2ef
Johnny JTH <johanohly@gmail.com>
2023-03-13 20:29:01 +0100

    Et step mere

--- src/routes/guide/+page.svelte
+++ src/routes/guide/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	let emailTemplate: HTMLPreElement;
-	let active: 'Info' | 'Step 1' | 'Step 2' = 'Info';
+	let active: 'Info' | 'Step 1' | 'Step 2' | 'Step 3' = 'Info';
 
 	const inactiveClasses = 'dark:border-gray-300 dark:text-gray-400';
 	const activeClasses = 'dark:border-violet-400 dark:text-gray-50';
@@ -8,7 +8,10 @@
 
 <svelte:head>
 	<title>Guide - ToldJS</title>
-	<meta name="description" content="Få hjælp til at komme i gang med at bruge vores platform. Her kan du finde en guide til at komme hurtigt i gang." />
+	<meta
+		name="description"
+		content="Få hjælp til at komme i gang med at bruge vores platform. Her kan du finde en guide til at komme hurtigt i gang."
+	/>
 </svelte:head>
 
 <div class="p-4 text-center md:px-10 lg:px-32 xl:max-w-3xl">
@@ -40,15 +43,21 @@
 				? activeClasses
 				: inactiveClasses}">Step 2</button
 		>
+		<button
+			on:click={() => (active = 'Step 3')}
+			class="px-2 py-1 border-b-2 md:border-l-2 md:border-b-0 md:py-3 {active == 'Step 3'
+				? activeClasses
+				: inactiveClasses}">Step 3</button
+		>
 	</div>
 	<div class="col-span-full md:col-span-4">
 		{#if active == 'Info'}
 			<h1 class="text-3xl font-bold">Info</h1>
 			<p>
 				Først skal du sende en email til din transportør om at du gerne vil selvfortolde. Derefter
-				skal du opsamle noget information om din pakke. Udfyld felterne i generatoren på siden, og
-				download PDF dokumentet. Til sidst skal du aflevere dokumentet til toldekspeditionen, og
-				betale tolden.
+				skal du opsamle noget information om din pakke. Brug denne information til at udfylde
+				felterne i generatoren på siden, og download PDF dokumentet. Til sidst skal du aflevere
+				dokumentet til toldekspeditionen, og betale tolden. Alt dette er beskrevet i de næste steps.
 			</p>
 		{:else if active == 'Step 1'}
 			<h1 class="text-3xl font-bold">Email</h1>
@@ -71,8 +80,23 @@ Mvh. NAVN
 				}}>Kopier</button
 			>
 		{:else if active == 'Step 2'}
+			<h1 class="text-3xl font-bold">Generator</h1>
+			<p>
+				Når du har fået svar fra transportøren, skal du samle nogle oplysninger om din pakke. Du
+				skal bruge disse oplysninger til at udfylde felterne i generatoren på siden. Når du har
+				udfyldt felterne, skal du trykke på knappen "Generer PDF", og så skal du downloade og printe
+				PDF dokumenterne. Du vil få ét dokument for hver vare i din pakke. Til sidst skal du
+				underskrive i bunden af side 6 og 7 på hvert dokument.
+			</p>
+			<a href="/generator" class="btn btn-primary" target="_blank" rel="noreferrer"
+				>Gå til generatoren</a
+			>
+		{:else if active == 'Step 3'}
 			<h1 class="text-3xl font-bold">Aflevering</h1>
-			<p>Nu hvor du har dine tolddokumenter skal du tage ned til en toldekspedition og aflevere papirerne. Her kan du se et kort over lokationerne:</p>
+			<p>
+				Nu hvor du har dine tolddokumenter skal du tage ned til en toldekspedition og aflevere
+				papirerne. Her kan du se et kort over lokationerne:
+			</p>
 			<iframe
 				title="Kort over toldekspeditioner"
 				src="https://www.google.com/maps/d/embed?mid=1-Wmnv0sU-QEMNZ11zr78HPwC11lWxjg&ehbc=2E312F"
