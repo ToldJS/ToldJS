a95e94ee620d4eb80eee73c8fbc797079e70a14e
Johnny JTH <johanohly@gmail.com>
2023-03-13 18:20:14 +0100

    Webhook test

--- src/routes/kontakt-os/sendt/+page.svelte
+++ src/routes/kontakt-os/sendt/+page.svelte
@@ -2,7 +2,7 @@
 	<div class="hero-content text-center">
 		<div class="max-w-md">
 			<h1 class="text-5xl font-bold">Tak for din besked!</h1>
-			<p class="py-2">Vi vender tilbage hurtigst muligt.</p>
+			<p class="py-2">Vi vender tilbage så hurtigt som muligt.</p>
 			<a href="/" class="btn btn-primary">Tilbage til forsiden</a>
 		</div>
 	</div>
