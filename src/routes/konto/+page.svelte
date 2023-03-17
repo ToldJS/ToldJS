a2f7e7b78f1edc6332ca4eb21b57c8063b0637eb
Johnny JTH <johanohly@gmail.com>
2023-03-17 13:32:25 +0100

    Note for database structure

--- src/routes/konto/+page.svelte
+++ src/routes/konto/+page.svelte
@@ -3,6 +3,17 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	// create table public.orders (
+	// 	id uuid not null primary key default uuid_generate_v4(),
+	// 	created_at timestamptz not null default now(),
+	// 	user_id uuid not null references auth.users on delete cascade,
+	// 	price int8 not null,
+
+	// 	primary key (id)
+	// );
+
+	// alter table public.orders enable row level security;
+
 </script>
 
 {#if data.session}
