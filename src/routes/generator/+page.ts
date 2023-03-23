67cfa25951a1b5ce3e2abecd94dd4f20bcb75fdf
Johnny JTH <johanohly@gmail.com>
2023-03-23 15:31:46 +0100

    Alle trin er færdige. Pænere login side.

--- src/routes/generator/+page.ts
+++ src/routes/generator/+page.ts
@@ -3,5 +3,5 @@ import type { PageLoad } from "./$types";
 export const load: PageLoad = (async ({ fetch }) => {
   const response = await fetch('https://openexchangerates.org/api/currencies.json');
   const data = await response.json();
-  return data;
+  return { currencies: data };
 });
