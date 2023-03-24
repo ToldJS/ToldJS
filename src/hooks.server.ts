490ff2ffa7c3fbc6048569e2279c965cda1f1820
Johnny JTH <johanohly@gmail.com>
2023-03-24 22:29:08 +0100

    Fjernet gamle tema hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,6 @@ import crypto from 'crypto';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SENTRY_DSN } from '$env/static/public';
 import type { Handle, HandleServerError } from '@sveltejs/kit';
-import { sequence } from '@sveltejs/kit/hooks';
 
 SentryNode.init({
     dsn: PUBLIC_SENTRY_DSN
@@ -21,8 +20,7 @@ export const handleError: HandleServerError = ({ error, event }) => {
     }
 }
 
-
-async function setupLocals({ event, resolve }) {
+export const handle: Handle = (async ({ event, resolve }) => {
     event.locals.supabase = createSupabaseServerClient({
         supabaseUrl: PUBLIC_SUPABASE_URL,
         supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
@@ -36,26 +34,4 @@ async function setupLocals({ event, resolve }) {
     };
 
     return resolve(event);
-}
-
-async function checkTheme({ event, resolve }) {
-    let theme: string | null = null;
-
-    const newTheme = event.url.searchParams.get('theme');
-    const cookieTheme = event.cookies.get('colortheme');
-
-    if (newTheme) {
-        theme = newTheme;
-    } else if (cookieTheme) {
-        theme = cookieTheme;
-    }
-    if (theme) {
-        return await resolve(event, {
-            transformPageChunk: ({ html }) => html.replace('data-theme=""', `data-theme="${theme}"`)
-        })
-    }
-
-    return resolve(event);
-}
-
-export const handle: Handle = sequence(setupLocals, checkTheme);
+});
