d0b1aae4ad8510e4bb66ce75dcdf50f3fb9d1e2a
Johnny JTH <johanohly@gmail.com>
2023-03-24 14:26:49 +0100

    Legal base

---
+++ src/routes/legal/+layout.svelte
@@ -0,0 +1,7 @@
+<div class="grid grid-cols-3 mb-12">
+    <a href="/legal" class="">Oversigt</a>
+    <a href="/legal/privatlivspolitik" class="">Privatlivspolitik</a>
+    <a href="/legal/servicevilkår" class="">Servicevilkår</a>
+</div>
+
+<slot />
--- src/routes/legal/+page.svelte
+++ src/routes/legal/+page.svelte
@@ -1,1 +1,12 @@
-Work in progress
+<div class="grid grid-cols-1 lg:grid-cols-2 lg:space-x-4">
+	<div class="card p-4 space-y-4">
+		<h3>Privatlivspolitik</h3>
+		<p>Forklarer hvordan vi behandler dine personlige data.</p>
+		<a href="/legal/privatlivspolitik">Læs vores privatlivspolitik</a>
+	</div>
+	<div class="card p-4 space-y-4">
+		<h3>Servicevilkår</h3>
+		<p>Beskriver vores forpligtelser overfor dig som bruger.</p>
+		<a href="/legal/servicevilkår">Læs vores servicevilkår</a>
+	</div>
+</div>
