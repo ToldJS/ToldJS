fc763cf295a98dd2ec7e48c579274e92a21113dc
Johnny JTH <johanohly@gmail.com>
2023-03-24 22:29:42 +0100

    Fjernet gammelt copyright

--- src/routes/om-os/+page.svelte
+++ src/routes/om-os/+page.svelte
@@ -6,6 +6,3 @@
 <p>ToldJS er en hjemmeside som har til formål at forenkle fortoldningsprocessen. Vores mål er at gøre det billigere at bestille vare fra udlandet, og undgå urimelige transportgebyrer. </p>
 <h1>Kontakt</h1>
 <p>ToldJS kan kontaktes på kontakt@toldjs.dk hvor vi bestræber os på at besvare hurtigst muligt</p>
-<h1>Logo fra:</h1>
-<a href="https://commons.wikimedia.org/wiki/File:Airport_Customs.svg">京市</a>,
-<a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>, via Wikimedia Commons
