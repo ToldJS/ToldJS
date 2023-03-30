0595349512a492347233cb9575cc6294e98dae67
Johnny JTH <johanohly@gmail.com>
2023-03-30 09:05:06 +0200

    Add 404 error handler

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,9 +2,18 @@
 	import { page } from '$app/stores';
 </script>
 
+<svelte:head>
+	<title>Fejl - ToldJS</title>
+</svelte:head>
+
 <div class="w-full text-center pt-12">
-	<span class="font-bold text-xl">Der skete en uventet fejl ({$page.status})</span> <br />
-	{#if $page.error?.errorId}
-		<span class="text-gray-500">Error ID: {$page.error.errorId}</span>
+	{#if $page.status === 404}
+		<span class="font-bold text-xl">Siden blev ikke fundet</span> <br />
+		<a href="/">Gå tilbage til forsiden</a>
+	{:else}
+		<span class="font-bold text-xl">Der skete en uventet fejl ({$page.status})</span> <br />
+		{#if $page.error?.errorId}
+			<span class="text-gray-500">Error ID: {$page.error.errorId}</span>
+		{/if}
 	{/if}
 </div>
